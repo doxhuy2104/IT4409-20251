4f711cf7177053bcc859c60698e5a492785a2a45
Ricky <huyha111204@gmail.com>
2025-11-07 20:02:58 +0700

    add tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="text-3xl font-bold underline">
+        Test Tailwind CSS with Vite and React
+      </div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +30,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
